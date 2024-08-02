b60e4c9aa1b706162edf54ed2902f01049cad89e
Hernan <hjviltez@gmail.com>
2024-08-02 11:49:27 -0300

    5/8/

--- src/main/java/med/voll/api/model/DatosListadoMedico.java
+++ src/main/java/med/voll/api/model/DatosListadoMedico.java
@@ -2,12 +2,13 @@ package med.voll.api.model;
 
 public record DatosListadoMedico(
 
+        Long id,
         String nombre,
         String email,
         String telefono,
         String especialidad) {
     public DatosListadoMedico(Medico medico){
-        this(medico.getNombre(), medico.getEmail(), medico.getTelefono(), medico.getEspecialidad().toString());
+        this(medico.getId(), medico.getNombre(), medico.getEmail(), medico.getTelefono(), medico.getEspecialidad().toString());
     }
 
 }
--- src/main/java/med/voll/api/model/IMedicoRepository.java
+++ src/main/java/med/voll/api/model/IMedicoRepository.java
@@ -1,6 +1,11 @@
 package med.voll.api.model;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.nio.channels.FileChannel;
+
 public interface IMedicoRepository extends JpaRepository<Medico, Long> {
+    Page<Medico> findByActivoTrue(Pageable paginacion);
 }
